2fb1aa34d634003921acb0f10b5daeda9b85efcc
v.dhanvin raj <parimalvelagapudi@gmail.com>
2026-04-03 21:36:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const admin = require("firebase-admin");
 // FIREBASE
 // =====================
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 const db = admin.firestore();
 
 // =====================
