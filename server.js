3ce3fce6a6b6242bc8b2a146969ad2dff1f46690
v.dhanvin raj <parimalvelagapudi@gmail.com>
2026-04-05 15:40:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -249,7 +249,14 @@ function requireJson(req, res, next) {
   next();
 }
 
-app.use(requireJson);
+app.use((req, res, next) => {
+  if (req.method !== "GET") {
+    if (!req.headers["content-type"] || !req.headers["content-type"].includes("application/json")) {
+      return res.status(415).json({ error: "Content-Type must be application/json" });
+    }
+  }
+  next();
+});
 
 // =====================
 // ROUTES
